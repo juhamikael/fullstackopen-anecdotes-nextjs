3af640ef4e72b2d9b789ece5c862299d422c327b
juhamikael <83360104+juhamikael@users.noreply.github.com>
2024-10-29 17:50:15 +0200

    Update index.ts

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -1,5 +1,12 @@
-import { config } from 'dotenv';
 import { drizzle } from 'drizzle-orm/neon-http';
-config({ path: '.env' });
+import { config } from 'dotenv';
 
-export const db = drizzle(process.env.DATABASE_URL!);
+if (process.env.NODE_ENV !== 'production') {
+    config({ path: '.env' });
+}
+
+if (!process.env.DATABASE_URL) {
+    throw new Error('DATABASE_URL is not set');
+}
+
+export const db = drizzle(process.env.DATABASE_URL);
